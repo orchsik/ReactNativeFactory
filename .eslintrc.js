a75c29a9a0fad4aebc2984b3f6dbf940ae480162
orchsik <orchsik@naver.com>
2021-10-05 09:51:55 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  rules: {
+    curly: 0,
+    quotes: 0,
+    'dot-notation': 0,
+    'no-unused-vars': 0,
+    '@typescript-eslint/no-unused-vars': 1,
+    'react-native/no-inline-styles': 0,
+    'react/self-closing-comp': 0,
+  },
+};
---
+++ src/library/index.ts
@@ -0,0 +1,5 @@
+const test = () => {
+  console.log(111);
+};
+
+export {test};
